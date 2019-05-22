235a4225e304c1ae4c1f030d5eac6903597dd4ad
u4bi <rootcode10@gmail.com>
2019-05-22 16:28:51 +0900

    feat: Added more routes

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import routes from './routes'
 
 const server = express()
 
 server
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({ extended : true }))
+    .use('/products', routes.products)
+    .use('/reviews', routes.reviews)
+    .use('/orders', routes.orders)
+    .use('/categories', routes.categories)
+    .use('/carts', routes.carts)
+    .use('/auths', routes.auths)
     .get('/', (req, res) => res.json({ message : 'test' }))
 
 export default server
---
+++ src/server/routes/index.js
@@ -0,0 +1,15 @@
+import products from './products'
+import reviews from './reviews'
+import orders from './orders'
+import categories from './categories'
+import carts from './carts'
+import auths from './auths'
+
+export default {
+    products,
+    reviews,
+    orders,
+    categories,
+    carts,
+    auths
+}
